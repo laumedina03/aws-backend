777996db5e798732656b05408c5751e1e9cf7c4d
Ubuntu <ubuntu@ip-172-31-20-247.ec2.internal>
2024-09-11 03:48:21 +0000

    Update POST request

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import mysql.connector
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})  # Allow all origins for testing
+CORS(app, resources={r"/*": {"origins": "http://ec2-35-174-170-11.compute-1.amazonaws.com:3000"}})  # Allow all origins for testing
 # Database connection setup
 def get_db_connection():
     return mysql.connector.connect(
@@ -27,6 +27,25 @@ def get_data():
     conn.close()
     return jsonify(rows)
 
+@app.route('/add-user',methods=['POST'])
+def add_user():
+    data = request.get_json()
+    first_name = data.get('first_name')
+    last_name = data.get('last_name')
+    birth_date = data.get('birth_date')
+    password = data.get('password')
+
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('''
+        INSERT INTO user (first_name, last_name, birth_date, password)
+        VALUES (%s, %s, %s, %s)
+    ''', (first_name, last_name, birth_date, password))
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+    return jsonify({'message': 'User added successfully!'}), 201
 if __name__ == '__main__':
     app.run(port=5000, host='0.0.0.0', debug=True)
 
