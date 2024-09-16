62757161e1cdab89c5fdb1c6ca59e116fce14fe9
laumedina03 <165229766+laumedina03@users.noreply.github.com>
2024-09-15 19:57:26 -0500

    Update register_test.py

--- tests/register_test.py
+++ tests/register_test.py
@@ -1,36 +1,20 @@
 import unittest
-import os
-from app import app, db, User  # Import the app, db, and User model
+from app import app  # Importa la aplicación Flask
 
 class TestUserRegistration(unittest.TestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        # Set the environment to testing
-        os.environ['FLASK_ENV'] = 'testing'
 
     def setUp(self):
         # Configurar un cliente de prueba para interactuar con la app Flask
         self.app = app.test_client()
         self.app.testing = True
 
-        # Set up the database for testing
-        with app.app_context():
-            db.create_all()
-    
-    def tearDown(self):
-        # Clean up after each test
-        with app.app_context():
-            db.session.remove()
-            db.drop_all()  # Drop all tables
-
     def test_register_user(self):
         # Datos de prueba
         new_user = {
-            'first_name': 'Ana',
-            'last_name': 'Gomez',
-            'birth_date': '1992-05-12',
-            'password': 'nueva123'
+            'first_name': 'Juan',
+            'last_name': 'Perez',
+            'birth_date': '1990-01-01',
+            'password': 'segura123'
         }
         response = self.app.post('/add-user', json=new_user)
         self.assertEqual(response.status_code, 201)
@@ -38,4 +22,3 @@ class TestUserRegistration(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-
