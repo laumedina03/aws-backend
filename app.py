4bbd5ed26641dd2187adfae1e9d90243c42f53fd
Ubuntu <ubuntu@ip-172-31-20-247.ec2.internal>
2024-09-16 03:57:57 +0000

    Update flask

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ from flask import Flask, jsonify, request
 import mysql.connector
 from flask_cors import CORS
 import requests
-import os
-import sqlite3
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "http://ec2-3-218-165-24.compute-1.amazonaws.com:3000"}})  # Allow all origins for testing
