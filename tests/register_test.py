a94d987479f45a0d6e50ef4fbc0ec4144a61c5d2
laumedina03 <165229766+laumedina03@users.noreply.github.com>
2024-09-16 23:12:53 -0500

    Update register_test.py

--- tests/register_test.py
+++ tests/register_test.py
@@ -14,20 +14,20 @@ class RegisterUserTestCase(unittest.TestCase):
 	cls.app.config['DATABASE'] = ':memory:'
 	cls.client = cls.app.test_client()
 
-    with cls.app.app_context():
-            conn = sqlite3.connect(':memory:')
-            cursor = conn.cursor()
+	with cls.app.app_context():
+	    conn = sqlite3.connect(':memory:')
+	    cursor = conn.cursor()
             cursor.execute('''
-		    CREATE TABLE IF NOT EXISTS user_test (
-			id INTEGER PRIMARY KEY AUTOINCREMENT,
-			first_name TEXT NOT NULL,
-			last_name TEXT NOT NULL,
-			birth_date TEXT NOT NULL,
-			password TEXT NOT NULL
-            )
-        ''')
-        conn.commit()
-        conn.close()
+		CREATE TABLE IF NOT EXISTS user_test (
+		    id INTEGER PRIMARY KEY AUTOINCREMENT,
+		    first_name TEXT NOT NULL,
+	            last_name TEXT NOT NULL,
+		    birth_date TEXT NOT NULL,
+	            password TEXT NOT NULL
+	         )
+	     ''')
+	     conn.commit()
+	     conn.close()
 
     def setUp(self):
         """Set up the database connection for each test."""
@@ -40,12 +40,12 @@ class RegisterUserTestCase(unittest.TestCase):
         self.conn.close()
 
     def test_register_user(self):
-       response = self.client.post('/add_user', json={
-                                     "first_name": "John",
-                                     "last_name": "Doe",
-                                     "birth_date": "1990-01-01",
-                                     "password": "password123"
-                                 })                           
+        response = self.client.post('/add_user', json={
+            "first_name": "John",
+            "last_name": "Doe",
+            "birth_date": "1990-01-01",
+            "password": "password123"
+        })                           
         self.assertEqual(response.status_code, 201)
 
     def test_get_users(self):
