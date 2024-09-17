f68ff8aa6a4ecd9b27b7434ec170e0c23739a4ec
laumedina03 <165229766+laumedina03@users.noreply.github.com>
2024-09-16 23:36:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,27 @@
 from flask import Flask, jsonify, request
 import mysql.connector
 from flask_cors import CORS
-import requests
+import sqlite3
+import os
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "http://ec2-3-218-165-24.compute-1.amazonaws.com:3000"}})  # Allow all origins for testing
+
 # Database connection setup
 def get_db_connection():
     if os.getenv('FLASK_ENV') == 'testing':
-        # Usar SQLite en memoria para pruebas
+        # Use SQLite in-memory for testing
         conn = sqlite3.connect(':memory:')
-        create_table_if_not_exists(conn)  # Crear la tabla al conectar
+        create_table_if_not_exists(conn)  # Create the table when connecting
         return conn
     else:
-       conn =  mysql.connector.connect(
+        conn = mysql.connector.connect(
             host='ec2-100-28-15-106.compute-1.amazonaws.com',  # Replace with your MySQL server's private IP
             user='camilin',            # Replace with your MySQL username
             password='1234',    # Replace with your MySQL password
             database='mysql_parcial'      # Replace with your database name
         )
-         return conn
+        return conn
 
 def create_table_if_not_exists(conn):
     cursor = conn.cursor()
@@ -49,7 +51,7 @@ def get_data():
     conn.close()
     return jsonify(rows)
 
-@app.route('/add-user',methods=['POST'])
+@app.route('/add-user', methods=['POST'])
 def add_user():
     data = request.get_json()
     first_name = data.get('first_name')
@@ -60,8 +62,7 @@ def add_user():
     conn = get_db_connection()
     cursor = conn.cursor()
 
-
-    # Usar diferentes marcadores de posición para SQLite y MySQL
+    # Use different placeholders for SQLite and MySQL
     if os.getenv('FLASK_ENV') == 'testing':
         query = 'INSERT INTO user (first_name, last_name, birth_date, password) VALUES (?, ?, ?, ?)'
     else:
@@ -77,26 +78,7 @@ def add_user():
     finally:
         cursor.close()
         conn.close()
-        
-
-# Function to test POST request for adding user
-"""def test_add_user():
-    url = 'http://ec2-54-158-25-160.compute-1.amazonaws.com:5000/add-user'
-    payload = {
-        "first_name": "Test",
-        "last_name": "Name",
-        "birth_date": "1990-01-01",
-        "password": "testpassword"
-    }
-    headers = {'Content-Type': 'application/json'}
-    response = requests.post(url, json=payload, headers=headers)
-
-    if response.status_code == 201:
-        print("Test passed: User added successfully")
-    else:
-        print(f"Test failed: Status code {response.status_code}")"""
-
-
 
 if __name__ == '__main__':
     app.run(port=5000, host='0.0.0.0', debug=True)
+
